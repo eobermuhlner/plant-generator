289a4e6b82fe5dbe3668ed06e101d8c16b1107c4
Eric Obermühlner <eobermuhlner@gmail.com>
2016-09-04 19:36:56 +0200

    unit test stochastic lsystem

--- ch.obermuhlner.plantgen.test/src/ch/obermuhlner/plantgen/lsystem/LindenmayerSystemTest.java
+++ ch.obermuhlner.plantgen.test/src/ch/obermuhlner/plantgen/lsystem/LindenmayerSystemTest.java
@@ -41,4 +41,29 @@ public class LindenmayerSystemTest {
 		assertEquals("{<[C]A>}", lindenmayerSystem.expand(random, "{A}"));
 	}
 
+
+	@Test
+	public void testStochastic() {
+		LindenmayerSystem lindenmayerSystem = new LindenmayerSystem();
+		Random random = new Random(1);
+		
+		lindenmayerSystem.addRule("A", 1, "1");
+		lindenmayerSystem.addRule("A", 9, "2");
+		
+		int n = 10000;
+		int count1 = 0;
+		int count2 = 0;
+		for (int i = 0; i < n; i++) {
+			String result = lindenmayerSystem.expand(random, "A");
+			if (result.equals("1")) {
+				count1++;
+			}
+			if (result.equals("2")) {
+				count2++;
+			}
+		}
+		
+		assertEquals(0.1, (double)count1 / n, 0.01);
+		assertEquals(0.9, (double)count2 / n, 0.01);
+	}
 }
