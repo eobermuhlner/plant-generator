dea798ff2badeb999af9b04b13be4953bf2c5cab
Eric Obermühlner <eobermuhlner@gmail.com>
2016-09-04 11:33:45 +0200

    cleanup imports

--- ch.obermuhlner.plantgen/src/ch/obermuhlner/plantgen/ui/turtle/command/LeafCommand.java
+++ ch.obermuhlner.plantgen/src/ch/obermuhlner/plantgen/ui/turtle/command/LeafCommand.java
@@ -5,7 +5,6 @@ import java.util.Deque;
 import ch.obermuhlner.plantgen.ui.turtle.TurtleCommand;
 import ch.obermuhlner.plantgen.ui.turtle.TurtleState;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.shape.StrokeLineCap;
 
 public class LeafCommand implements TurtleCommand {
 
