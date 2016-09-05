6928a4a7773bfe12b8529c0f228c4479406101e7
Eric Obermühlner <eobermuhlner@gmail.com>
2016-09-05 07:31:40 +0200

    cleanup imports

--- ch.obermuhlner.plantgen/src/ch/obermuhlner/plantgen/lsystem/LindenmayerSystem.java
+++ ch.obermuhlner.plantgen/src/ch/obermuhlner/plantgen/lsystem/LindenmayerSystem.java
@@ -3,11 +3,10 @@ package ch.obermuhlner.plantgen.lsystem;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.Random;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LindenmayerSystem {
 
