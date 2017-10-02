aa91c4d03f50c06477a9d4a880fd5074a5c23f25
Eric Obermühlner <eobermuhlner@gmail.com>
2017-10-02 23:02:16 +0200

    offsets for leaf color gradient

--- ch.obermuhlner.plantgen/src/ch/obermuhlner/plantgen/ui/turtle/command/LeafCommand.java
+++ ch.obermuhlner.plantgen/src/ch/obermuhlner/plantgen/ui/turtle/command/LeafCommand.java
@@ -15,15 +15,19 @@ public class LeafCommand implements TurtleCommand {
 
 	private final Color leaf1Color;
 	private final Color leaf2Color;
+	private final double colorCenterOffset;
+	private final double color2Offset;
 	private final double leafSize;
 	private final double leafThicknessFactor;
 	private final double leafLengthFactor;
 	private final double leafWidthFactor;
 	private final double widthAngle;
 
-	public LeafCommand(Color leaf1Color, Color leaf2Color, double leafSize, double leafThicknessFactor, double leafLengthFactor, double leafWidthFactor, double widthAngle) {
+	public LeafCommand(Color leaf1Color, Color leaf2Color, double colorCenterOffset, double color2Offset, double leafSize, double leafThicknessFactor, double leafLengthFactor, double leafWidthFactor, double widthAngle) {
 		this.leaf1Color = leaf1Color;
 		this.leaf2Color = leaf2Color;
+		this.colorCenterOffset = colorCenterOffset;
+		this.color2Offset = color2Offset;
 		this.leafSize = leafSize;
 		this.leafThicknessFactor = leafThicknessFactor;
 		this.leafLengthFactor = leafLengthFactor;
@@ -49,6 +53,7 @@ public class LeafCommand implements TurtleCommand {
 
 		double length = getLeafLength(state);
 		double width = getLeafWidth(state);
+		double colorCenterLength = length * colorCenterOffset;
 		
 		double dxLeft = width * Math.cos(state.angle - widthAngle);
 		double dyLeft = width * Math.sin(state.angle - widthAngle);
@@ -59,7 +64,10 @@ public class LeafCommand implements TurtleCommand {
 		double dxLength = length * Math.cos(state.angle);
 		double dyLength = length * Math.sin(state.angle);
 
-		RadialGradient radialGradient = new RadialGradient(0, 0, state.x2d, state.y2d, length, false, CycleMethod.NO_CYCLE, new Stop(0.5, leaf1Color), new Stop(1.0, leaf2Color));
+		double dxColorCenter = colorCenterLength * Math.cos(state.angle);
+		double dyColorCenter = colorCenterLength * Math.sin(state.angle);
+		
+		RadialGradient radialGradient = new RadialGradient(0, 0, state.x2d + dxColorCenter, state.y2d + dyColorCenter, length, false, CycleMethod.NO_CYCLE, new Stop(0.0, leaf1Color), new Stop(color2Offset, leaf2Color));
 		gc.setFill(radialGradient);
 
 		gc.beginPath();
